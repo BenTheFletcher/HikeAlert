ace705745ba0f13babde13e5ac23e2c9bb45e492
Ben Fletcher <thebenfletcher@gmail.com>
2019-02-24 14:14:36 -0700

    Initial Release

---
+++ MyApplication/app/src/main/java/com/hikealert/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.hikealert.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.*;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+    public void goToEmergencyOptions (View view) {
+        Intent intent = new Intent(this, emergencyOptions.class);
+        startActivity(intent);
+    }
+    public void goTouserMap (View view){
+        Intent intent = new Intent (this, Map_User.class);
+        startActivity(intent);
+    }
+    public void goToAboutPage (View view){
+        Intent intent = new Intent (this, AboutPage.class);
+        startActivity(intent);
+    }
+
+}
