3fd438569a9730d8da97b528adc5ec72800c0ee8
Ben Fletcher <thebenfletcher@gmail.com>
2019-03-11 11:39:51 -0700

    Update

--- MyApplication/app/src/main/java/com/hikealert/myapplication/ConfirmPage.java
+++ MyApplication/app/src/main/java/com/hikealert/myapplication/ConfirmPage.java
@@ -5,14 +5,39 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class ConfirmPage extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_page);
-    }
+
+
+            Calendar c = Calendar.getInstance();
+            System.out.println("Current time => "+c.getTime());
+
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String formattedDate = df.format(c.getTime());
+            // formattedDate have current date/time
+            Toast.makeText(this, formattedDate, Toast.LENGTH_SHORT).show();
+
+
+            // Now we display formattedDate value in TextView
+            TextView txtView = new TextView(this);
+            txtView.setText("Current Date and Time:\n"+formattedDate);
+            txtView.setGravity(Gravity.CENTER);
+            txtView.setTextSize(20);
+            setContentView(txtView);
+        }
 
 }
+
+
--- MyApplication/app/src/main/java/com/hikealert/myapplication/emergencyOptions.java
+++ MyApplication/app/src/main/java/com/hikealert/myapplication/emergencyOptions.java
@@ -19,5 +19,8 @@ public class emergencyOptions extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
+    public void goToConfirmPage (View view) {
+        Intent intent = new Intent(this, ConfirmPage.class);
+        startActivity(intent);
+    }
 }
